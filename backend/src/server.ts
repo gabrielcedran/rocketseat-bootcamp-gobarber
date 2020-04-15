96f7f18e138746e25d4c3043fec1e285120b0169
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-15 12:56:37 +0100

    Create endpoint to serve static content from express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import uploadConfig from "./config/upload";
 import "./database";
 import "reflect-metadata";
 
 const app = express();
 
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.destinationPath));
 app.use(routes);
 
 app.listen(3333, () => {
