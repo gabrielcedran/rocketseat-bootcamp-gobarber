9535a93d36ca0135e32d66e04ac3ee6c29f1cae2
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-23 18:34:43 +0100

    Save logged in user in the localstorage, add user data in the auth context (app context) so that other components have access to it and load logged in users from localstorage

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import goBarberApi from '../services/goBarberApi';
 
 interface SignInCredentials {
@@ -7,8 +7,13 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name?: string;
+  user?: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+interface AuthState {
+  token: string;
+  user: object;
 }
 
 // In order to centrilize the login function, inside the context being created, provide the login method
@@ -21,13 +26,28 @@ export const AuthContext = createContext<AuthContextData>(
 // Instead of creating it arbitrary on the highest component, isolate it in a specialized component and simple use it where it
 // has to be used.
 export const AuthProvider: React.FC = ({ children }) => {
+  const [authData, setAuthData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await goBarberApi.post('/sessions', { email, password });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setAuthData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user: authData.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
