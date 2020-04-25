a729c64b910bf26569ec1043160c42676bb4b7b9
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-25 21:37:49 +0100

    Introduce navigation between signin and signup

--- native/src/pages/SignIn/SignIn.tsx
+++ native/src/pages/SignIn/SignIn.tsx
@@ -7,6 +7,7 @@ import {
   Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import {
   Container,
   Title,
@@ -19,54 +20,57 @@ import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => (
-  <>
-    {/** Android has this behavious by default */}
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      enabled
-    >
-      <ScrollView
-        contentContainerStyle={{ flex: 1 }}
-        keyboardShouldPersistTaps="handled"
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+  return (
+    <>
+      {/** Android has this behavious by default */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        <Container>
-          <Image source={logoImg} />
-          {/** Hack for IOS - when applying the keyboard avoiding view, the screen slides upwords, but this sliding animation is now applicable
-           * to Texts isolated
-           */}
-          <View>
-            <Title>Faça o seu login</Title>
-          </View>
-          <Input name="email" icon="mail" placeholder="E-mail" />
-          <Input name="password" icon="lock" placeholder="Senha" />
-          <Button
-            onPress={() => {
-              console.log('Teste');
-            }}
-          >
-            Entrar
-          </Button>
-          <ForgotPassword
-            onPress={() => {
-              console.log('forgot password!');
-            }}
-          >
-            <ForgotPasswordText>Esqueci a minha senha</ForgotPasswordText>
-          </ForgotPassword>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
-    <CreateAccount
-      onPress={() => {
-        console.log('create account!');
-      }}
-    >
-      <Icon name="log-in" size={20} color="#ff9000" />
-      <CreateAccountText>Criar uma conta</CreateAccountText>
-    </CreateAccount>
-  </>
-);
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            {/** Hack for IOS - when applying the keyboard avoiding view, the screen slides upwords, but this sliding animation is now applicable
+             * to Texts isolated
+             */}
+            <View>
+              <Title>Faça o seu login</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('Teste');
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword
+              onPress={() => {
+                console.log('forgot password!');
+              }}
+            >
+              <ForgotPasswordText>Esqueci a minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccount
+        onPress={() => {
+          navigation.navigate('SignUp');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Criar uma conta</CreateAccountText>
+      </CreateAccount>
+    </>
+  );
+};
 
 export default SignIn;
--- native/src/pages/SignUp/SignUp.tsx
+++ native/src/pages/SignUp/SignUp.tsx
@@ -7,6 +7,7 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import {
   Container,
   Title,
@@ -18,34 +19,37 @@ import Button from '../../components/Button/Button';
 
 import logo from '../../assets/logo.png';
 
-const SignUp: React.FC = () => (
-  <>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      enabled
-    >
-      <ScrollView
-        contentContainerStyle={{ flex: 1 }}
-        keyboardShouldPersistTaps="handled"
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        <Container>
-          <Image source={logo} />
-          <View>
-            <Title>Crie sua conta</Title>
-          </View>
-          <Input name="name" icon="users" placeholder="Nome" />
-          <Input name="email" icon="mail" placeholder="E-mail" />
-          <Input name="password" icon="lock" placeholder="Senha" />
-          <Button>Cadastrar</Button>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
-    <ReturnLogin>
-      <Icon name="arrow-left" style={{ color: '#f4ede8' }} size={16} />
-      <ReturnLoginText>Voltar para logon</ReturnLoginText>
-    </ReturnLogin>
-  </>
-);
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+            <Input name="name" icon="users" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <ReturnLogin onPress={() => navigation.navigate('SignIn')}>
+        <Icon name="arrow-left" style={{ color: '#f4ede8' }} size={16} />
+        <ReturnLoginText>Voltar para logon</ReturnLoginText>
+      </ReturnLogin>
+    </>
+  );
+};
 
 export default SignUp;
